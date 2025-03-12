aee594f9323331b8d4202f22b925c4172b1884f5
xkodxdf <sirota92@bk.ru>
2025-03-12 14:25:25 +0300

    fix[exchange rates values]

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -5,6 +5,7 @@ CREATE TABLE currency
     code      VARCHAR(3)  NOT NULL,
     full_name VARCHAR(48) NOT NULL
 );
+
 CREATE UNIQUE INDEX currency_code_index
     ON currency (code, full_name);
 
@@ -19,21 +20,24 @@ CREATE TABLE exchange_rate
     base_currency_id   INTEGER        NOT NULL REFERENCES currency (id) ON DELETE CASCADE,
     target_currency_id INTEGER        NOT NULL REFERENCES currency (id) ON DELETE CASCADE,
     rate               DECIMAL(10, 6) NOT NULL
-
 );
 
 CREATE UNIQUE INDEX exchange_rate_base_target_index
     ON exchange_rate (base_currency_id, target_currency_id);
 
 INSERT INTO exchange_rate(base_currency_id, target_currency_id, rate)
-VALUES ((SELECT id FROM currency WHERE code = 'RUB'),
-        (SELECT id FROM currency WHERE code = 'USD'),
+VALUES ((SELECT id FROM currency WHERE code = 'USD'),
+        (SELECT id FROM currency WHERE code = 'RUB'),
         87.6967),
 
        ((SELECT id FROM currency WHERE code = 'RUB'),
-        (SELECT id FROM currency WHERE code = 'EUR'),
-        92.0362),
+        (SELECT id FROM currency WHERE code = 'USD'),
+        0.0116),
+
+       ((SELECT id FROM currency WHERE code = 'EUR'),
+        (SELECT id FROM currency WHERE code = 'RUB'),
+        93.6639),
 
        ((SELECT id FROM currency WHERE code = 'USD'),
         (SELECT id FROM currency WHERE code = 'EUR'),
-        0.96);
+        0.9242);
