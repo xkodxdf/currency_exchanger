3b8d9466e835d290cfdf97f9c8c7d7a5d26e49ea
xkodxdf <sirota92@bk.ru>
2025-03-24 20:02:43 +0300

    add[exchange at a reverse rate]

--- src/main/java/com/xkodxdf/app/service/ExchangeService.java
+++ src/main/java/com/xkodxdf/app/service/ExchangeService.java
@@ -1,13 +1,17 @@
 package com.xkodxdf.app.service;
 
+import com.xkodxdf.app.ErrorMessage;
 import com.xkodxdf.app.dto.ExchangeRateRequestDto;
 import com.xkodxdf.app.dto.ExchangeRequestDto;
 import com.xkodxdf.app.dto.ExchangeResponseDto;
+import com.xkodxdf.app.exception.DataNotFoundException;
 import com.xkodxdf.app.model.dao.interfaces.ExchangeRateDao;
 import com.xkodxdf.app.model.entity.ExchangeEntity;
 import com.xkodxdf.app.model.entity.ExchangeRateEntity;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
 
 public class ExchangeService extends BaseService {
 
@@ -23,5 +27,48 @@ public class ExchangeService extends BaseService {
         BigDecimal amountToExchange = new BigDecimal(requestDtoToExchange.amount());
         ExchangeEntity exchangeEntity = new ExchangeEntity(exchangeRate, amountToExchange);
         return new ExchangeResponseDto(exchangeEntity);
+    }
+
+    private ExchangeRateEntity findExchangeRate(ExchangeRateRequestDto requestDto) {
+        return findDirectExchangeRate(requestDto)
+                .or(() -> findReversedExchangeRate(requestDto))
+                .orElseThrow(() -> new DataNotFoundException(ErrorMessage.NOT_FOUND_ERR));
+    }
+
+    private Optional<ExchangeRateEntity> findDirectExchangeRate(ExchangeRateRequestDto requestDto) {
+        try {
+            return Optional.of(exchangeRateDao.get(requestDto));
+        } catch (DataNotFoundException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<ExchangeRateEntity> findReversedExchangeRate(ExchangeRateRequestDto requestDto) {
+        try {
+            ExchangeRateRequestDto invertedRequestDto = createInvertedExchangeRateRequestDto(requestDto);
+            ExchangeRateEntity exchangeRate = exchangeRateDao.get(createInvertedExchangeRateRequestDto(invertedRequestDto));
+            return Optional.of(createInvertedExchangeRate(exchangeRate));
+        } catch (DataNotFoundException e) {
+            return Optional.empty();
+        }
+    }
+
+    private ExchangeRateRequestDto createInvertedExchangeRateRequestDto(ExchangeRateRequestDto requestDto) {
+        return new ExchangeRateRequestDto(
+                requestDto.targetCurrencyCode(),
+                requestDto.baseCurrencyCode()
+        );
+    }
+
+    private ExchangeRateEntity createInvertedExchangeRate(ExchangeRateEntity exchangeRate) {
+        int scale = 4;
+        long idForNotPresentedRates = -1L;
+        BigDecimal invertedRate = BigDecimal.ONE.divide(exchangeRate.rate(), scale, RoundingMode.HALF_EVEN);
+        return new ExchangeRateEntity(
+                idForNotPresentedRates,
+                exchangeRate.targetCurrency(),
+                exchangeRate.baseCurrency(),
+                invertedRate
+        );
     }
 }
