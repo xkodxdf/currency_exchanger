4bcc7ff3e693729a78de7e4be8145fe15af4435e
xkodxdf <sirota92@bk.ru>
2025-03-24 20:00:33 +0300

    add[new err messages]

--- src/main/java/com/xkodxdf/app/ErrorMessage.java
+++ src/main/java/com/xkodxdf/app/ErrorMessage.java
@@ -10,7 +10,12 @@ public final class ErrorMessage {
     public static final String DUPLICATION_ERR = "This data has already been added earlier";
     public static final String NOT_FOUND_ERR = "Unable to find data";
     public static final String REQUIRED_PARAMS_ERR = "All field must be filled with correct data";
+    public static final String AMOUNT_TO_CONVERT_TOO_SMALL_ERR = "Amount to convert must be greater then 0.01";
     public static final String INVALID_CURRENCY_CODE = "Currency code must consist of three latin letters (ex. USD)";
+    public static final String INVALID_CURRENCY_NAME = "Currency name may contain latin letters and spaces, " +
+                                                       "maximum length 48 characters";
+    public static final String BIG_DECIMAL_CONVERSION_ERR = "Incorrect data. Enter an integer or decimal number with a dot. " +
+                                                            "(ex. 112, 44.55, 0.011";
     public static final String INVALID_NUMERIC_STRING_LENGTH = "Maximum number of characters for exchange rate " +
                                                                "or amount to exchange 17";
     public static final String INVALID_EXCHANGE_RATE_CODE_ERR = "Exchange rate code should consist of two currency codes," +
