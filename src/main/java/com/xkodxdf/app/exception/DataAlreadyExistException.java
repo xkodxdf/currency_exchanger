945c242c67d4774656982e134a580ecb4b6d7dea
xkodxdf <sirota92@bk.ru>
2025-04-04 12:07:36 +0300

    ref[remove unused constructors]

--- src/main/java/com/xkodxdf/app/exception/DataAlreadyExistException.java
+++ src/main/java/com/xkodxdf/app/exception/DataAlreadyExistException.java
@@ -4,19 +4,7 @@ import com.xkodxdf.app.ErrorMessage;
 
 public class DataAlreadyExistException extends CurrencyExchangerException {
 
-    public DataAlreadyExistException() {
-        super(ErrorMessage.DUPLICATION_ERR);
-    }
-
     public DataAlreadyExistException(Throwable t) {
         super(ErrorMessage.DUPLICATION_ERR, t);
-    }
-
-    public DataAlreadyExistException(String message) {
-        super(message);
-    }
-
-    public DataAlreadyExistException(String message, Throwable t) {
-        super(message, t);
     }
 }
--- src/main/java/com/xkodxdf/app/exception/DataNotFoundException.java
+++ src/main/java/com/xkodxdf/app/exception/DataNotFoundException.java
@@ -4,19 +4,11 @@ import com.xkodxdf.app.ErrorMessage;
 
 public class DataNotFoundException extends CurrencyExchangerException {
 
-    public DataNotFoundException() {
-        super(ErrorMessage.NOT_FOUND_ERR);
-    }
-
     public DataNotFoundException(Throwable t) {
         super(ErrorMessage.NOT_FOUND_ERR, t);
     }
 
     public DataNotFoundException(String message) {
         super(message);
-    }
-
-    public DataNotFoundException(String message, Throwable t) {
-        super(message, t);
     }
 }
